a69646788b54a00c1dc3542672830fcb9e6481f7
Anubhab Mondal <62478707+SmartAnubhab@users.noreply.github.com>
2021-09-25 06:10:45 +0000

    tested printing integer as string working fine

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 name='Anubhab'
 lastName='Mondal'
 fullName=name + lastName
-
+age = 25
 
 print( 'My name is {} \nand my age is {age} '.format(name, age=27))
 
+print(f'My full name is {fullName} and I am {age} years old')
