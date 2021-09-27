8c5922906677d326366579de4c93d54ed72fecc4
Anubhab Mondal <62478707+SmartAnubhab@users.noreply.github.com>
2021-09-27 14:10:30 +0000

    append

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-li = [
-  ['one', 'two', 'three'],
-  ['four','five','six'],
-  ['seven','eight','nine'],
-]
-print(f'printing \'li\' - {li}\n')
+li = ['one','two','three','four']
 
-print(li[0][0])
-print(li[0][1])
-print(li[0][2])
-print(li[1][0])
-print(li[1][1])
-print(li[1][2])
-print(li[2][0])
-print(li[2][1])
-print(li[2][2])
+li.append('five')
 
-matrix = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-]
-print(f'\nprinting \'matrix\' - {matrix}\n')
-
-print(matrix[0][0])
-print(matrix[0][1])
-print(matrix[0][2])
-print(matrix[1][0])
-print(matrix[1][1])
-print(matrix[1][2])
-print(matrix[2][0])
-print(matrix[2][1])
-print(matrix[2][2])
+print(li)
 
 
 
