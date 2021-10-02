adf438b3db5675e0838c29859c2b7ec034bfabc6
Anubhab Mondal <62478707+SmartAnubhab@users.noreply.github.com>
2021-10-02 16:09:19 +0000

    usage of if else and boolen values in python

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-li = ['one','two','three','four']
+is_magician =  False
+is_expert = False
 
-li.append('five')
-
-print(li)
-
-
+if is_magician :
+  if is_expert :
+    print('you are a master magician')
+  else :
+    print('at least you are getting there')
+else :
+  print('you need magic powers')
 
