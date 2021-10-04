7442a57a0ec99560f01c7e2b5eeab99cd558831b
Anubhab Mondal <62478707+SmartAnubhab@users.noreply.github.com>
2021-10-04 07:28:37 +0000

    pixels game using 2D array

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-user = {
-  'NAME' : 'ANUBHAB MONDAL',
-  'ROLL' : 33,
-  'SECTION' : 'A'
-}
+picture = [
+  [0,0,0,1],
+  [0,0,1,1],
+  [0,1,1,1],
+  [1,1,1,1]
+]
 
-for item in user.items():
-  print(item)
+for row in picture:
+  for pixels in row:
+    if pixels == 1:
+      print('* ' , end='')
+    else:
+      print(' ' , end='' )
+  print()
 
-  
+
+
+
